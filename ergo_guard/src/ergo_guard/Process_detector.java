439516fa5e317849045c95d6ec10144a53b91ca5
Antonio Chavez <cavjzz@gmail.com>
2012-03-27 12:50:12 -0600

    Refactored ergo guard main class in a subclass process detector

---
+++ ergo_guard/src/ergo_guard/Process_detector.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+package ergo_guard;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Process_detector {
+  private Process_detector() {;}
+
+  public static File writeVbScript(){
+    try{
+      //Se crea el archivo vb script en temp
+      File script = File.createTempFile("realhowto",".vbs");
+      script.deleteOnExit();
+      FileWriter writeScript = new java.io.FileWriter(script);
+
+      //Se llena el archivo vb (estas son las istricciones en en codigo basic para acceder al TASKLIST.EXE)
+      String vbs = "Set WshShell = WScript.CreateObject(\"WScript.Shell\")\n"
+        + "Set locator = CreateObject(\"WbemScripting.SWbemLocator\")\n"
+        + "Set service = locator.ConnectServer()\n"
+        + "Set processes = service.ExecQuery _\n"
+        + " (\"select name from Win32_Process\")\n"
+        + "For Each process in processes\n"
+        + "wscript.echo process.Name \n"
+        + "Next\n"
+        + "Set WSHShell = Nothing\n";
+      writeScript.write(vbs);
+      writeScript.close();
+      return script;
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  public static List<string> executeScript(File script){
+    try{
+      List<String> processList = new ArrayList<String>();
+      //Java ejecuta el script
+      Process p = Runtime.getRuntime().exec("cscript //NoLogo " + file.getPath());
+      System.out.println("This is the path: " + file.getPath() + "\n");
+      BufferedReader input =
+        new BufferedReader
+        (new InputStreamReader(p.getInputStream()));
+
+      //Java captura las lineas de informacion que proporciona el script
+      String line;
+      while ((line = input.readLine()) != null) {
+        System.out.println("The process: " + line + "\n");
+        processList.add(line);
+      }
+      input.close();
+      return processList;
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  public static List<String> listRunningProcesses() {
+    try {
+      file = Process_detector.writeVbScript();
+      Process_detector.excecuteScript(file);
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+}
